6cb21d651d5d0f6922aadb46042af9fff33dad12
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2023-10-19 21:46:04 -0300

    feat: create a custom outline to form components

--- src/components/Sidebar/NavItem.tsx
+++ src/components/Sidebar/NavItem.tsx
@@ -10,7 +10,7 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
   return (
     <a
       href=""
-      className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
+      className="group flex items-center gap-3 rounded px-3 py-2 outline-none hover:bg-violet-50 focus:border-violet-300 focus:ring-4 focus:ring-violet-100"
     >
       <Icon className="h-5 w-5 text-zinc-500" />
       <span className="font-medium text-zinc-700 group-hover:text-violet-500">
